15f99632c3d83dfde7c6e84a6d9b13a08bf346b5
jobensingh <jobensingh@users.noreply.github.com>
2017-08-27 18:22:50 -0700

    Update main.go

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main() {
 		newMessage  := randomdata.Country(randomdata.FullCountry)
 		noAdmin := true
 		
-		for index := s.GuildMembers(g, _, 1000) {
+		for index, element := range s.GuildMembers(g, _, 1000) {
 			for index2 := s.index.Roles {
 				if(index2.Name := "God") {
 					noAdmin = false
